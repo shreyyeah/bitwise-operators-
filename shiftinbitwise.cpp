814a2681f8879c9ed40acfbbce14c355ddda4ae6
SHREYA NARAYAN <140142783+shreyyeah@users.noreply.github.com>
2023-10-27 22:34:17 +0530

    Add files via upload

---
+++ shiftinbitwise.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main()
+{
+    using namespace std;
+   int x,y,a,b;
+   x=67; //input values
+   a=x<<3; /* '<<' operator */
+   y=9;
+   b=y>>4;/* '>>' operator */
+   cout<<"shift left:"<<a<<endl;
+   cout<<"shift right:"<<b<<endl;
+
+}
