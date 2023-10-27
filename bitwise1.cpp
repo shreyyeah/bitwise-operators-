d3df661d727ed0d143e8614dcf2f113646e591f7
SHREYA NARAYAN <140142783+shreyyeah@users.noreply.github.com>
2023-10-27 22:29:12 +0530

    Add files via upload

---
+++ bitwise1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main()
+{
+    using namespace std;
+   int i,a,b;
+   i=35;
+   a=35|16;/* '|' operator*/
+   b=35&223;/* '&' operator*/
+   cout<<"or:"<<a<<endl;
+   cout<<"and:"<<b<<endl;
+
+}
